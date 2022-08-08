0f91917c043c19a23c5e6d77540f1f670140fae7
Stephen Pal-George <stephenpalgeorge@gmail.com>
2022-08-08 07:02:18 +0100

    gulp watch working for dev command

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ const gulp = require('gulp');
 const njk = require('gulp-nunjucks-render');
 const sass = require('gulp-sass')(require('sass'));
 
-gulp.task("markup", function () {
+gulp.task("markup", () => {
     return gulp
         .src('src/pages/*')
         .pipe(njk({ path: ['src/templates/'] }))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task("styles", function () {
+gulp.task("styles", () => {
     return gulp
         .src('src/styles/*')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('dist/styles'));
 });
 
+gulp.task("dev", () => {
+    gulp.watch('src/**/*', gulp.series(["markup", "styles"]));
+});
 gulp.task("default", gulp.series(["markup", "styles"]));
