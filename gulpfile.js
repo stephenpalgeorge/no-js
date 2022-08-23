e7ff45dfb82d8631abfc29c76edf03204e60c5c3
Stephen Pal-George <stephenpalgeorge@gmail.com>
2022-08-23 15:42:00 +0100

    nav component v1 working

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 const gulp = require('gulp');
 const njk = require('gulp-nunjucks-render');
 const sass = require('gulp-sass')(require('sass'));
+const data = require('gulp-data');
+const dataFile = require('./src/data.json');
 
 gulp.task("markup", () => {
     return gulp
         .src('src/pages/**/*')
+        .pipe(data(dataFile))
         .pipe(njk({ path: ['src/templates/', 'src/partials/'] }))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task("styles", () => {
     return gulp
-        .src('src/styles/*')
+        .src(['src/styles/**/*.scss', 'src/partials/components/**/*.scss'])
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('dist/styles'));
 });
